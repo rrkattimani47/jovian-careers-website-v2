dc28b7d06398655a52c2176790222bc37103ddd4
rrkattimani47 <rrkattimani47@gmail.com>
2023-12-29 04:37:32 +0000

    added ssl

--- database.py
+++ database.py
@@ -1,5 +1,14 @@
-import sqlalchemy
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
-engine = create_engine("mysql+pymysql://wqgvegatku7sa7dxk55y:pscale_pw_bhlab9IBzXp3awoRxPGItJ9Lo770wtGXrQGJA6gkuKc@aws.connect.psdb.cloud/rashmicareers?charset=utf8mb4")
+db_connection_string = "mysql+pymysql://wqgvegatku7sa7dxk55y:pscale_pw_bhlab9IBzXp3awoRxPGItJ9Lo770wtGXrQGJA6gkuKc@aws.connect.psdb.cloud/rashmicareers?charset=utf8mb4"
 
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                           "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
+
+with engine.connect() as connection:
+  result = connection.execute(text("select * from jobs"))
+  print(type(result))
+  print(result.all())
+  
