36535884c60503e0b4dfcb79aad4a455ace522c5
rrkattimani47 <rrkattimani47@gmail.com>
2023-12-29 04:24:19 +0000

    connected to mysql database via planetscale

---
+++ database.py
@@ -0,0 +1,5 @@
+import sqlalchemy
+from sqlalchemy import create_engine
+
+engine = create_engine("mysql+pymysql://wqgvegatku7sa7dxk55y:pscale_pw_bhlab9IBzXp3awoRxPGItJ9Lo770wtGXrQGJA6gkuKc@aws.connect.psdb.cloud/rashmicareers?charset=utf8mb4")
+
